2a0b2f9f30edcf89da8e8df2be2d56a1f1e62f23
Polk, Scott W <scott.polk@pearson.com>
2015-09-15 10:09:02 -0500

    Placeable objects

---
+++ Assets/Scripts/PlacementScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementScript : MonoBehaviour {
+
+	private Collider2D pinPointCollider = null;
+	private bool isPinned = false;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPinned)
+			return;
+
+		if (pinPointCollider != null && Input.GetMouseButtonDown(0)) {
+			Vector3 offset = (pinPointCollider as CircleCollider2D).offset;
+			transform.position = offset + pinPointCollider.gameObject.transform.position;
+			isPinned = true;
+			return;
+		}
+
+		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		float posZ = transform.position.z - Camera.main.transform.position.z;
+		Vector3 newPosition = new Vector3(
+			position.x,
+			position.y,
+			0.0f);
+/*		Vector3 newPosition = new Vector3(
+			Mathf.Floor(position.x),
+			Mathf.Floor(position.y),
+			0.0f);
+*/		transform.position = newPosition;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.CompareTag("Pin")) {
+			collider.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+			pinPointCollider = collider;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider) {
+		collider.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+		pinPointCollider = null;
+	}
+}
