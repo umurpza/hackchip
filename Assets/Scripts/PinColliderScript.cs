e82acfe34483600d4682fb99ca830200731c9bc6
Polk, Scott W <scott.polk@pearson.com>
2015-09-16 13:02:10 -0500

    Added security chip placement

---
+++ Assets/Scripts/PinColliderScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinColliderScript : MonoBehaviour {
+
+	private Collider2D pinPointCollider = null;
+	private bool isPinned = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPinned)
+			return;
+		
+		if (pinPointCollider != null && Input.GetMouseButtonDown(0)) {
+			Vector3 pinOffset = transform.parent.position - transform.position;
+
+			Vector3 offset = (pinPointCollider as CircleCollider2D).offset;
+//			transform.position = offset + pinPointCollider.gameObject.transform.position;
+
+			transform.parent.gameObject.GetComponent<PlacementScript>().enabled = false;
+
+			Vector3 v1 = offset + pinPointCollider.gameObject.transform.position;
+			Vector3 v2 = v1 + pinOffset;
+
+			transform.parent.position = v2;
+
+			isPinned = true;
+			return;
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log(collider.gameObject.transform.childCount);
+		if (collider.gameObject.CompareTag("OpenPin")) {
+			Transform parent = collider.gameObject.transform;
+			foreach (Transform child in parent) {
+				Debug.Log(child.name);
+				child.gameObject.SetActive(true);
+			}
+			pinPointCollider = collider;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D collider) {
+		if (collider.gameObject.transform.childCount > 0) {
+			foreach (Transform child in collider.gameObject.transform) {
+				child.gameObject.SetActive(false);
+			}
+		}
+		
+		pinPointCollider = null;
+	}
+}
--- Assets/Scripts/PlacementScript.cs
+++ Assets/Scripts/PlacementScript.cs
@@ -2,10 +2,7 @@
 using System.Collections;
 
 public class PlacementScript : MonoBehaviour {
-
-	private Collider2D pinPointCollider = null;
-	private bool isPinned = false;
-
+	
 	// Use this for initialization
 	void Start () {
 		transform.position = Input.mousePosition;
@@ -13,40 +10,14 @@ public class PlacementScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (isPinned)
-			return;
-
-		if (pinPointCollider != null && Input.GetMouseButtonDown(0)) {
-			Vector3 offset = (pinPointCollider as CircleCollider2D).offset;
-			transform.position = offset + pinPointCollider.gameObject.transform.position;
-			isPinned = true;
-			return;
-		}
-
 		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		float posZ = transform.position.z - Camera.main.transform.position.z;
 		Vector3 newPosition = new Vector3(
 			position.x,
 			position.y,
 			0.0f);
-/*		Vector3 newPosition = new Vector3(
-			Mathf.Floor(position.x),
-			Mathf.Floor(position.y),
-			0.0f);
-*/		transform.position = newPosition;
+
+		transform.position = newPosition;
 	}
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.gameObject.CompareTag("Pin")) {
-			collider.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-			pinPointCollider = collider;
-		}
-	}
-
-	void OnTriggerExit2D(Collider2D collider) {
-		if (collider.gameObject.transform.childCount > 0)
-			collider.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-
-		pinPointCollider = null;
-	}
 }
