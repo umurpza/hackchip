7370b56b8dad9bf04d295f7f11e22cc269d356d9
Polk, Scott W <scott.polk@pearson.com>
2015-09-16 22:25:06 -0500

    Completed PoC

--- Assets/Scripts/FinishScript.cs
+++ Assets/Scripts/FinishScript.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class FinishScript : MonoBehaviour {
 
-	public GameObject bug;
+	public GameObject powahSwitch;
 	public PointsCounter pointsCounter;
+	public int points;
 
 	public void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.CompareTag("Bug"))
-			pointsCounter.UpdatePoints(10);
+		if (collider.CompareTag("Bug")) {
+
+			if (collider.gameObject.name.Equals("redbug2")) {
+				powahSwitch.GetComponent<Powah>().StartBug3();
+			}
+			else
+			{
+				powahSwitch.transform.Rotate(Vector3.forward * 180);
+			}
+
+			pointsCounter.UpdatePoints(points);
+			Destroy (collider.gameObject);
+		}
 	}
 
 }
---
+++ Assets/Scripts/Powah.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Powah : MonoBehaviour {
+
+	public GameObject bug1;
+	public GameObject bug2;
+	public GameObject bug3;
+	//	public TargetWalker targetWalkerScript;
+	private bool firstRun = true;
+
+	void OnMouseDown() {
+		if (Input.GetMouseButtonDown(0)) {
+			
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+			
+			if (hit.collider != null) {
+				AudioSource click = gameObject.GetComponent<AudioSource>();
+				click.Play();
+
+				transform.Rotate(Vector3.forward * 180);
+
+				StartBugs();
+			}
+		}
+	}
+
+	private void StartBugs() {
+		if (firstRun)
+		{
+			TargetWalker script = bug1.GetComponent<TargetWalker>();
+			script.Run();
+			firstRun = false;
+		}
+		else
+		{
+			TargetWalker script = bug2.GetComponent<TargetWalker>();
+			script.Run();
+		}
+	}
+
+	public void StartBug3() {
+		TargetWalker script = bug3.GetComponent<TargetWalker>();
+		script.Run();
+	}
+
+}
